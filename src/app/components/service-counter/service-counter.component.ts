6f5000699f2e803ab514f4844c2f8e53a425f09d
Wolfgang Goritschnig <goritschnig.wolfgang@fronius.com>
2022-11-14 15:39:53 +0100

    sets up workspace II

---
+++ src/app/components/service-counter/service-counter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-service-counter',
+  templateUrl: './service-counter.component.html',
+  styleUrls: ['./service-counter.component.sass']
+})
+export class ServiceCounterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
